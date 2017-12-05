482695a4b291bc393416e85e64bb8a8540d32645
kellmacartney <33106247+kellmacartney@users.noreply.github.com>
2017-12-05 14:46:00 +1000

    Splash Screen
    Splash screen added with logos and buttons.

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void LoadLevel(string NewGamelvl)
+    {
+        SceneManager.LoadScene(NewGamelvl);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
