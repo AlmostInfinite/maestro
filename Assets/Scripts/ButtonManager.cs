8a30fb0e0e846aabec64a9426f3bc57d9be0c06a
kellmacartney <33106247+kellmacartney@users.noreply.github.com>
2017-12-05 17:24:16 +1000

    Menu| Credits| splash
    Main menu, credits and splash panels implemented

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -3,16 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class ButtonManager : MonoBehaviour
 {
-    public void LoadLevel(string NewGamelvl)
+    public GameObject CreditsPanel;
+    public GameObject MenuPanel;
+    public GameObject SplashPanel;
+
+    bool credits = false;
+    bool menu = false;
+    bool splash = true;
+
+    void Update()
     {
-        SceneManager.LoadScene(NewGamelvl);
+        if (credits)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                ShowCredits();
+            }
+        }
+        if (menu)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                ShowMenu();
+            }
+        }
+        if (splash)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                ShowSplash();
+            }
+        }
     }
+    public void ShowCredits()
+    {
+        credits = true;
+        splash = false;
+        menu = false; 
+        CreditsPanel.SetActive(true);
+        MenuPanel.SetActive(false);
+        SplashPanel.SetActive(false);
+    }
+
+    public void ShowMenu()
+    {
+        menu = true;
+        credits = false;
+        splash = false;
+        MenuPanel.SetActive(true);
+        SplashPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+
+    }
+
+    public void ShowSplash()
+    {
+        splash = true; 
+        credits = false;
+        menu = false;
+        SplashPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        MenuPanel.SetActive(false);
+    }
+
 
     public void QuitGame()
     {
         Application.Quit();
     }
-
+   
 }
