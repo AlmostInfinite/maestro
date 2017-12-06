aef8abbe8bb92a761cac3e806609dfef0f4523ce
kellmacartney <33106247+kellmacartney@users.noreply.github.com>
2017-12-06 20:18:28 +1000

    Score UI
    Score Panel UI implemented for all levels
    waiting on scoring funciontality to finish it up

---
+++ Assets/Scripts/ScorePanelManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScorePanelManager : MonoBehaviour
+{
+
+    //score script stuff up here
+
+    bool Onestar = false;
+    bool TwoStar = false;
+    bool ThreeStar = false;
+
+    public GameObject PanelOne;
+    public GameObject PanelTwo;
+    public GameObject PanelThree;
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Onestar)
+        {
+            ShowPanelOne();
+        }
+
+        if (TwoStar)
+        {
+            ShowPanelTwo();
+        }
+
+        if (ThreeStar)
+        {
+            ShowPanelThree();
+        }
+	}
+
+
+    public void ShowPanelOne ()
+    {
+        PanelOne.SetActive(true);
+    }
+
+    public void ShowPanelTwo()
+    {  
+        PanelTwo.SetActive(true);
+    }
+
+    public void ShowPanelThree()
+    {
+        PanelThree.SetActive(true);
+    }
+
+    public void ReturnToMenu(string LevelToLoad)
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+}
